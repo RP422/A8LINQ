044434ce826247e27e1f03402d29c2fb6bfbeda6
Mike Lang <mlang@student.neumont.edu>
2016-08-16 08:41:57 -0600

    Questions done up to Q03
    Signed-off-by: Mike Lang <mlang@student.neumont.edu>

---
+++ Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Assignment
+{
+    public static class Extensions
+    {
+        //Q01
+        public static void PrintResults(this IEnumerable<Student> list)
+        {
+            foreach (Student s in list)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace LINQ_Assignment
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var firstQuery = Student.students.Where(FilterGarcia);
+            firstQuery.PrintResults();
+
+            //Q03
+            var secondQuery = from student in Student.students
+                              where student.Last == "Garcia"
+                              select student;
+            secondQuery.PrintResults();
+        }
+
+        //Q02
+        private static bool FilterGarcia(Student s)
+        {
+            return s.Last == "Garcia";
         }
     }
 }
